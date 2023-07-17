b36a6439faa2a2c78182acbaa7f8593b58d86f37
do262431 <do262431@gmail.com>
2023-07-17 19:46:22 -0400

    Internal camera access aquired

--- add_faces.py
+++ add_faces.py
@@ -1,3 +1,12 @@
 import cv2
 
-cv2.VideoCapture(0)
+video = cv2.VideoCapture(0) # internal webcam = 0  external webcam = 1
+
+while True:
+    ret, frame = video.read()
+    cv2.imshow("frame", frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
