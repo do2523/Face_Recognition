e582e085b9f71896d1f487ed6775b653af91da37
do262431 <do262431@gmail.com>
2023-07-17 19:51:12 -0400

    Convert to gray_scale picture to make it easier

--- add_faces.py
+++ add_faces.py
@@ -1,9 +1,11 @@
 import cv2
 
 video = cv2.VideoCapture(0) # internal webcam = 0  external webcam = 1
+faces = cv2.CascadeClassifier('./data/harcasscade.xml')
 
 while True:
     ret, frame = video.read()
+    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("frame", frame)
     k = cv2.waitKey(1)
     if k == ord('q'):
