1bf6b0804c007861fcebfd5a181095079809e41d
do262431 <do262431@gmail.com>
2023-07-17 20:10:40 -0400

    Add box to for face recognition

--- add_faces.py
+++ add_faces.py
@@ -1,12 +1,15 @@
 import cv2
 
 video = cv2.VideoCapture(0) # internal webcam = 0  external webcam = 1
-faces = cv2.CascadeClassifier('./data/harcasscade.xml')
+face_detect = cv2.CascadeClassifier('./data/harcasscade.xml')
 
 while True:
     ret, frame = video.read()
     gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("frame", frame)
+    faces = face_detect.detectMultiScale(gray_scale, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (50,50,255),3)
+    cv2.imshow("Frame", frame)
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
